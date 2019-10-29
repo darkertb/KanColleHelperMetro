7caea3903446191c4e5635e95288a8a9c6172bbe
DarkerTB <david911206@gmail.com>
2019-10-30 01:56:46 +0800

    1

---
+++ booklive-merge.js
@@ -0,0 +1,33 @@
+if (!window.mergeImported) {
+  $('body').append('<script src="https://unpkg.com/merge-images"></script>')
+  $('#content_base').append(`<button id="mergeBtn" style="position:absolute;height:30px" onclick="merge();">merge</button>`)
+  window.mergeImported = true
+}
+
+function merge () {
+  let page = $('#menu_slidercaption').text().split('/')[0]
+  let imgSize = []
+  let imgUrl = []
+  let _width = 0
+  let _height = 0
+  for (let i = 0; i < 3; i++) {
+    let img = $('#content-p' + page + ' img')[i]
+
+    imgSize.push([img.width, img.height])
+    imgUrl.push({ src: img.src, x: 0, y: _height })
+
+    _width = +imgSize[i][0]
+    _height += +imgSize[i][1]
+  }
+
+  mergeImages(imgUrl, { width: _width, height: _height })
+    .then(b64 => {
+      let hasAppend = $('#customDownload').length > 0
+      if (hasAppend) {
+        $('#customDownload').attr('src', b64).text(`download page:${page}`)
+      }
+      else {
+        $('#content_base').append(`<image id="customDownload" src="${b64}" style="position:absolute;top:30px;width:100px" download>download page:${page}</image>`)
+      }
+    });
+}
